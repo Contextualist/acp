7e9a1c35aab2f0e45d840936d672f3f74109f393
Contextualist <harzjc@gmail.com>
2023-01-16 19:11:09 -0800

    tweak: pnet: HTTPClient carrys over default transport options
    e.g. This should make our client aware of $HTTPS_PROXY
    although proxy seems to have compatibility issue with hole-punching

--- pkg/pnet/httpclient.go
+++ pkg/pnet/httpclient.go
@@ -16,19 +16,23 @@ func NewHTTPClient(useIpv6 bool) *HTTPClient {
 		Client:  &http.Client{},
 		chLaddr: make(chan net.Addr, 1),
 	}
-	tr := &http.Transport{
-		DialContext: func(ctx context.Context, _network, addr string) (net.Conn, error) {
-			network := "tcp4"
-			if useIpv6 {
-				network = "tcp6"
-			}
-			c, err := DialContext(ctx, network, ":0", addr)
+
+	tr := *(http.DefaultTransport.(*http.Transport)) // create a value copy of the default
+	tr.DialContext = func(ctx context.Context, _network, addr string) (net.Conn, error) {
+		network := "tcp4"
+		if useIpv6 {
+			network = "tcp6"
+		}
+		c, err := DialContext(ctx, network, ":0", addr)
+		if err == nil {
 			client.chLaddr <- c.LocalAddr()
-			return c, err
-		},
-		ForceAttemptHTTP2: true,
+		} else {
+			defaultLogger.Debugf("pnet.HTTPClient.Transport.DialContext: %v", err)
+			client.chLaddr <- &net.TCPAddr{}
+		}
+		return c, err
 	}
-	client.Client.Transport = tr
+	client.Client.Transport = &tr
 	return client
 }
 
